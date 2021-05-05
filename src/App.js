06cdb0f49d8a588455aefb9b4016a9cae97c1804
IsSabuhi <sabuhi.israfilov2001@gmail.com>
2021-05-05 16:43:59 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import { AppBar, Container, IconButton, Toolbar, Typography, Box } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+import {makeStyles} from '@material-ui/core/styles';
+import { red } from '@material-ui/core/colors';
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginRight: theme.spacing(1)
+  },
+  title: {
+    flexGrow: 1
+  }
+}))
+
+
+function App() {
+  const classes = useStyles();
+  return (
+    <AppBar position="fixed">
+      <Container fixed>
+        <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="menu" className={classes.menuButton}>
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6"className={classes.title}>Web Developer Blog</Typography>
+          <Box mr={3}>
+            <Button color="inherit" variant="outlined">Log In</Button>
+          </Box>
+          <Button color="secondary" variant="contained">Sign Up</Button>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+
+export default App;
